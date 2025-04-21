94e6c2cd39e40a96600b8748ab37938e4587fc3b
makarr2024 <romanov.m.a.3@gmail.com>
2025-04-21 22:07:53 +0500

    Update seconds.py

--- seconds.py
+++ seconds.py
@@ -1,15 +1,30 @@
 # напиши модуль для реализации секундомера
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.properties import BooleanProperty, NumericProperty, StringProperty
 
 class Seconds(Label):
-   
+    done = BooleanProperty(False)
     def __init__(self, total, **kwargs):
-        pass
-
-    def restart(self, total, **kwargs):
-        pass
+        self.total = total
+        self.current = 0
+        self.done = False
+        self.timer_text = 'Секунд прошло:' + str(self.current)
+        super().__init__(text=self.timer_text)
 
     def start(self):
-        pass
+        Clock.schedule_interval(self.change, 1)
 
     def change(self, dt):
-        pass
+        self.current += 1
+        self.text = 'Секунд прошло:' + str(self.current)
+        if self.current >= self.total:
+            self.done = True
+            return False
+
+    def restart(self, total, **kwargs):
+        self.current = 0
+        self.total= total
+        self.done = False
+        self.timer_text = 'Секунд прошло:' + str(self.current)
+        self.start()
