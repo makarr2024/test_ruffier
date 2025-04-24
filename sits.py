7d215d878932f10f598a1221be4e101a89ae8cd5
makarr2024 <romanov.m.a.3@gmail.com>
2025-04-24 23:01:09 +0500

    Update sits.py

--- sits.py
+++ sits.py
@@ -1,9 +1,15 @@
 # напиши модуль для подсчета количества приседаний
+from kivy.uix.label import Label
 
 class Sits(Label):
     
     def __init__(self, total, **kwargs):
-        pass
+        self.sits_now = 0
+        self.total = total
+        sits_rem_txt = 'Осталось приседаний:' + str(total)
+        super().__init__(text=sits_rem_txt, **kwargs)
 
     def next(self, *args):
-        pass
+        self.current += 1
+        remain = max(0, self.total - self.current)
+        self.text = 'Осталось приседаний:' + str(remain)
